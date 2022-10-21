62cd3132671b9b239c72bb7d9c4c995eeae1325b
jumavictor <victorjuma48@gmail.@gmail.com>
2022-10-21 01:22:08 -0700

    Task 3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 || year % 4 == 0) && !(year % 100 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -34,7 +34,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
-return (0);
-
 }
